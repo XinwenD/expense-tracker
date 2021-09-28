a9082f6077515e7eea706a4facb891fddc94bd96
xwdongo@gmail.com <xwdongo@gmail.com>
2021-09-28 16:10:04 -0700

    webpack the react app expense tracker

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,46 @@
-const resolve = require("path");
+const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "production";
 
 module.exports = {
   entry: "./src/index.js",
-
   output: {
     filename: "built.js",
     path: resolve(__dirname, "build"),
   },
   module: {
     rules: [
-      // loader的配置
       {
-        // 处理less资源
-        test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"],
-      },
-      {
-        // 处理css资源
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
       {
-        // 处理图片资源
-        test: /\.(jpg|png|gif)$/,
-        loader: "url-loader",
-        options: {
-          limit: 8 * 1024,
-          name: "[hash:10].[ext]",
-          // 关闭es6模块化
-          esModule: false,
-          outputPath: "imgs",
-        },
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
       },
       {
-        // 处理html中img资源
-        test: /\.html$/,
-        loader: "html-loader",
-      },
-      {
-        // 处理其他资源
-        exclude: /\.(html|js|css|less|jpg|png|gif)/,
+        exclude: /\.(js|css|less|html|jpg|png|gif)/,
         loader: "file-loader",
         options: {
-          name: "[hash:10].[ext]",
           outputPath: "media",
         },
       },
     ],
   },
+  resolve: {
+    // modules: [path.resolve(__dirname, "./src"), "node_modules"],
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
   plugins: [
-    // plugins的配置
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./public/index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
     }),
   ],
   mode: "development",
-  devServer: {
-    contentBase: resolve(__dirname, "build"),
-    compress: true,
-    port: 3000,
-    open: true,
-  },
 };
